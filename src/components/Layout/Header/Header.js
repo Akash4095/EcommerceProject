46a6f348a53ca9a8fd71121d3b5667a271e0c1c2
Akash4095 <94811251+Akash4095@users.noreply.github.com>
2022-01-20 20:51:52 +0530

    Add files via upload

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,10 +1,10 @@
 import React,{useContext} from 'react';
 import './Header.css'
 // import CartList from '../../Cart/CartList'
-
+import { NavLink } from "react-router-dom";
 import {Link} from 'react-router-dom';
 import { Cart } from "../../StoreContext/CartContext";
-
+import './Navlink.css'
 
 const Header = (props)=>{
 
@@ -13,9 +13,9 @@ const Header = (props)=>{
     return (
         <header>
             <ul className='header'>
-               <li><a href="/homepage">HOME</a></li>
-               <li><Link to="/">STORE</Link></li> 
-               <li><Link to="/about" >ABOUT</Link></li>
+               <li><NavLink className={(navData) => navData.isActive ? "active" : "" } to="/">STORE</NavLink></li>
+               <li><NavLink className={(navData) => navData.isActive ? "active" : "" } to="/">STORE</NavLink></li> 
+               <li><NavLink className={(navData) => navData.isActive ? "active" : "" } to="/about">ABOUT</NavLink></li>
                <button className='btn' onClick={props.showCartItem}>"cart" <span className='cart-number'>{cart.length}</span></button>
             </ul>
             <h1>The Generics</h1>
